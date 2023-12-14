e360cf52fd6ebe56f6a1c6edcb640aa9547c2778
Geoff Powell <gpowell@squareup.com>
2023-12-13 20:11:11 -0500

    Merge branch 'add-list-delete' of github.com:geoff-powell/tidy into add-list-delete
    * 'add-list-delete' of github.com:geoff-powell/tidy:
      Add delete function for list
      Cleanup edit list

--- common/src/commonMain/kotlin/com/greenmiststudios/common/persistance/Databases.kt
+++ common/src/commonMain/kotlin/com/greenmiststudios/common/persistance/Databases.kt
@@ -3,9 +3,19 @@ package com.greenmiststudios.common.persistance
 import com.greenmiststudios.common.data.TidyList
 import com.greenmiststudios.tidy.TidyListQueries
 
+
+public val USER_ID: String = "USER"
+
 public fun TidyListQueries.updateList(tidyList: TidyList) {
   updateListName(name = tidyList.name, id = tidyList.id)
   tidyList.items.forEach {
-    addOrUpdateListItem(it.id, tidyList.id, it.text, it.completed,"USER")
+    addOrUpdateListItem(it.id, tidyList.id, it.text, it.completed, USER_ID)
+  }
+}
+
+public fun TidyListQueries.createList(tidyList: TidyList) {
+  addList(id = tidyList.id, name = tidyList.name, "", USER_ID)
+  tidyList.items.forEach {
+    addOrUpdateListItem(it.id, tidyList.id, it.text, it.completed,USER_ID)
   }
 }

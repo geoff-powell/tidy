85eb5aa4f23abc060dff4db18c794afc0ad773e8
Geoff Powell <gpowell@squareup.com>
2023-12-18 06:41:40 -0500

    Add spotless and editor-config

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,12 +20,25 @@ plugins {
   alias(libs.plugins.kotlin.android) apply false
   alias(libs.plugins.ksp) apply false
   alias(libs.plugins.sqldelight) apply false
+  alias(libs.plugins.spotless) apply false
 }
 
 allprojects {
   afterEvaluate {
     extensions.findByType<ComposeExtension>()?.apply {
       kotlinCompilerPlugin.set(libs.versions.compose.kotlin.compiler.get())
+    }
+
+    extensions.findByType<com.diffplug.gradle.spotless.SpotlessExtension>()?.apply { // if you are using build.gradle.kts, instead of 'spotless {' use:
+      kotlin {
+        // by default the target is every '.kt' and '.kts` file in the java sourcesets
+        ktlint()   // has its own section below
+        // licenseHeader '/* (C)$YEAR */' // or licenseHeaderFile
+      }
+      kotlinGradle {
+        // target '*.gradle.kts' // default target for kotlinGradle
+        ktlint() // or ktfmt() or prettier()
+      }
     }
   }
 }
